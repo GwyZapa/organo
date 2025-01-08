c16ef06a43b7fd5180ba693d86e79e76e3679f99
Guilherme Zapater <guizapa368@gmail.com>
2025-01-08 15:35:02 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Banner from './components/Banner/Banner';
+import CampoTexto from './components/CampoTexto/CampoTexto';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      <CampoTexto></CampoTexto>
+    </div>
+  );
+}
+
+export default App;
