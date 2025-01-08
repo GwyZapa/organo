486709979e6a6539c9dd4627869fc3d03b8ad1c6
Guilherme Zapater <guizapa368@gmail.com>
2025-01-08 15:55:14 -0300

    componente de texto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Banner from './components/Banner/Banner';
-import CampoTexto from './components/CampoTexto/CampoTexto';
+import Banner from './components/Banner';
+import CampoTexto from './components/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <CampoTexto></CampoTexto>
+      <CampoTexto label="Nome"/>
+      <CampoTexto label="Cargo"/>
+      <CampoTexto label="Imagem"/>
+
     </div>
   );
 }
