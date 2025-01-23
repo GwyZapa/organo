7371a8639e8630c831ab96ef557ffb0e391bf873
Guilherme Zapater <guizapa368@gmail.com>
2025-01-23 15:39:31 -0300

    listener dos colaboradores + componente time

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -4,7 +4,7 @@ import './Formulario.css'
 import Botao from '../Botao'
 import { useState } from 'react'
 
-const Formulario = () => {
+const Formulario = (props) => {
 
     const times = [
         'Programação',
@@ -15,17 +15,24 @@ const Formulario = () => {
         'Mobile',
         'Inovação e Gestão'
     ]
-    
+
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
-    const [time, setTime] =useState('')
+    const [time, setTime] = useState('')
 
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Form submetido!', nome, cargo, imagem, time);
-        
+
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+            }
+        )
+
     }
 
     return (
@@ -33,11 +40,11 @@ const Formulario = () => {
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o Card de Seu Personagem</h2>
                 <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu Nome" valor={nome} aoAlterado={valor => setNome(valor)} />
-                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu Cargo" valor={cargo} aoAlterado={valor => setCargo(valor)}/>
-                <CampoTexto    label="Imagem" placeholder="Digite o endereço da Imagem" valor={imagem} aoAlterado={valor => setImagem(valor)}/>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu Cargo" valor={cargo} aoAlterado={valor => setCargo(valor)} />
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da Imagem" valor={imagem} aoAlterado={valor => setImagem(valor)} />
                 <ListaSuspensa obrigatorio={true} label="Time " itens={times} valor={time} aoAlterado={valor => setTime(valor)}  ></ListaSuspensa>
                 <Botao>
-                        Criar Card
+                    Criar Card
                 </Botao>
             </form>
         </section>
